b5b8837de7a025cf48f59a025f452f96dc14aa4e
Edoardo Ruiz <44981900+edoardrui0@users.noreply.github.com>
2024-03-20 11:04:03 -0400

    starter loop has been added
    commented out the playGame() func to work on the randomization loop
    of the cpu

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ function getComputerChoice() {
     return randomChoice;
 }
 
+// current for loop that is being worked on. So far it is allowing me to be able to fully randomize cpu selection
+for (let i=0; i<=5; i++) {
+    getComputerChoice().toLowerCase();
+    console.log(getComputerChoice())
+}
+
 // this creates a conditional to determine if the player is a winner, loser, or if there is a tie 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
@@ -21,76 +27,75 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-
-// const playerSelection = 'scissors'.toLowerCase();
-const computerSelection = getComputerChoice().toLowerCase();
-
-// console.log(playerSelection)
-// console.log(computerSelection)
-
-
-// console.log(playRound(playerSelection, computerSelection))
-
 // This will keep track of scores
 let userScore = 0;
 let cpuScore = 0;
 
+
+// ***new comment*** this function was commented out to work on the randomization of the cpu selection
 // this function allows me to play the game in a single round. I haven't figured out how to do multiple rounds just yet
-function playGame() {
+// function playGame() {
 
-    // commented out lines 62-93 to try something new
-    // this new method below allows me to keep track of score, need to work on loops
-    let userChoice = prompt(`Let's play Rock, Paper, Scissors!`)
-    let result = playRound(userChoice, computerSelection);
+//     // commented out lines 62-93 to try something new
+//     // this new method below allows me to keep track of score, need to work on loops
 
-    if ( result == `It's a tie!` ) {
-        console.log(`You and the CPU tied. Try again!`)
-    } else if ( result == `You lose` ) {
-        cpuScore++
-        console.log(`You have lost to the CPU. You have ${userScore} points. The CPU has ${cpuScore} points`)
-    } else if ( result == `You win!` ) {
-        userScore++
-        console.log(`You have won against the CPU. You have ${userScore} points. The CPU has ${cpuScore} points`)
-    }
+//     for ( let i=1; i<=5; i++ ) { 
 
-    if ( cpuScore == 5 ) {
-        console.log(`You have lost. Tough luck`)
-    } else if ( userScore == 5 ) {
-        console.log(`You have won! Go celebrate or something, idk`)
-    }
+//         let userChoice = prompt(`Let's play Rock, Paper, Scissors!`).toLowerCase();
+//         let result = playRound(userChoice, computerSelection);
+        
+//         if ( result == `It's a tie!` ) {
+//             console.log(`You and the CPU tied. Try again!`)
+//             userChoice;
+//         } else if ( result == `You lose` ) {
+//             cpuScore++
+//             console.log(`You have lost to the CPU. You have ${userScore} points. The CPU has ${cpuScore} points`)
+//             userChoice;
+//         } else if ( result == `You win!` ) {
+//             userScore++
+//             console.log(`You have won against the CPU. You have ${userScore} points. The CPU has ${cpuScore} points`)
+//             userChoice;
+//         }
 
-    // function keepScore() {
-        // let userScore = 0;
-        // let cpuScore = 0;
+//         if ( cpuScore == 3 ) {
+//             console.log(`You have lost. Tough luck`)
+//         } else if ( userScore == 3 ) {
+//             console.log(`You have won! Go celebrate or something, idk`)
+//         } 
+//     }
+
+//     // function keepScore() {
+//         // let userScore = 0;
+//         // let cpuScore = 0;
     
-    //     for ( let i=0; i<5; i++) {
-    //         userScore++;
-    //         cpuScore++;
-    //     }
-    // }
+//     //     for ( let i=0; i<5; i++) {
+//     //         userScore++;
+//     //         cpuScore++;
+//     //     }
+//     // }
 
-    // let userChoice = prompt(`Let's play Rock, Paper, Scissors!`)
-    // let userScore = 0;
-    // let cpuScore = 0;
+//     // let userChoice = prompt(`Let's play Rock, Paper, Scissors!`)
+//     // let userScore = 0;
+//     // let cpuScore = 0;
 
-    // for ( let i=1; i<=5; i++ ) {
+//     // for ( let i=1; i<=5; i++ ) {
         
-    //     let result = playRound(userChoice, computerSelection);
+//     //     let result = playRound(userChoice, computerSelection);
        
-    //     if ( result == `It's a tie!` ) {
-    //         console.log(`You and the CPU have tied. Try again!`)
-    //     }  else if ( result == `You lose` ) {
-    //         console.log(`You have lost to the CPU. A point for the CPU!`)
-    //         cpuScore++
-    //         console.log(cpuScore)
-    //     }  else if ( result == `You win!` ) {
-    //         console.log(`You have won! A point for you!`)
-    //         userScore++
-    //         console.log(userScore)
-    //     }
+//     //     if ( result == `It's a tie!` ) {
+//     //         console.log(`You and the CPU have tied. Try again!`)
+//     //     }  else if ( result == `You lose` ) {
+//     //         console.log(`You have lost to the CPU. A point for the CPU!`)
+//     //         cpuScore++
+//     //         console.log(cpuScore)
+//     //     }  else if ( result == `You win!` ) {
+//     //         console.log(`You have won! A point for you!`)
+//     //         userScore++
+//     //         console.log(userScore)
+//     //     }
 
         
-    // }
-}
+//     // }
+// }
 
-console.log(playGame());
+// playGame();
